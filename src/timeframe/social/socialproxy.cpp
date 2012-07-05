bc5e5dd6c70ef45618b84bf91feb34e93df8c4a5
Sergey Borovkov <sergey.borovkov@osinit.ru>
2012-07-05 15:03:28 +0400

    Reparent model in social proxy to this

--- src/timeframe/social/socialproxy.cpp
+++ src/timeframe/social/socialproxy.cpp
@@ -9,21 +9,21 @@
 SocialProxy::SocialProxy(QList<ISocialModule *> plugins, QObject *parent) :
     QObject(parent), m_plugins(plugins), m_model(0)
 {
-    ISocialModule *plugin = m_plugins[0];
-    connect(plugin, SIGNAL(authorizationStatusChanged(int)), SLOT(authenticated()));
-    connect(plugin->requestManager(), SIGNAL(newSocialItems(QList<SocialItem*>)), SLOT(newItems(QList<SocialItem*>)));
+    connect(plugins[0], SIGNAL(authorizationStatusChanged(int)), SLOT(authenticated()));
+    connect(plugins[0]->requestManager(), SIGNAL(newSocialItems(QList<SocialItem*>)), SLOT(newItems(QList<SocialItem*>)));
 //    QWidget *w = plugin->authenticationWidget();
 //    w->show();
 }
 
 SocialProxy::~SocialProxy()
 {
-    delete m_model;
+
 }
 
 void SocialProxy::setModel(SocialModel *model)
 {
     m_model = model;
+    m_model->setParent(this);
     m_plugins[0]->requestManager()->queryWall(QDate(), QDate());
 }
 
@@ -34,7 +34,6 @@ SocialModel *SocialProxy::model() const
 
 void SocialProxy::authenticated()
 {
-    qDebug() << "desu desu";
     m_plugins[0]->requestManager()->queryWall(QDate(), QDate());
 }
 
