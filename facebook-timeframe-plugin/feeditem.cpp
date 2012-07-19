3da1a2dca5b4e74524df86d204166cecc36fe810
Sergey Borovkov <sergey.borovkov@osinit.ru>
2012-07-19 17:13:10 +0400

    Add support (basic) for "stories" on facebook and for links

--- facebook-timeframe-plugin/feeditem.cpp
+++ facebook-timeframe-plugin/feeditem.cpp
@@ -29,9 +29,26 @@ void FeedItem::fillFromMap(QVariantMap map)
     if(map.contains("id"))
         m_id = map.value("id").toString();
     if(map.contains("message"))
-        m_data.insert(Text, map.value("message").toString());
+    {
+        QString message = map.value("message").toString();
+
+        // if user posts a link
+        bool isLink = message.startsWith("http://");
+        if(isLink)
+        {
+            QString name = isLink ? map.value("name").toString() : message;
+            m_data.insert(Text, QString("<a href=%1>%2</a>").arg(message, name));
+        }
+        else
+            m_data.insert(Text, message);
+    }
     if(map.contains("picture"))
         m_data.insert(Image, map.value("picture").toString());
+
+    if(map.contains("story"))
+    {
+        m_data.insert(Text, map.value("story").toString());
+    }
 
     m_data.insert(PluginName, pluginName());
 }
