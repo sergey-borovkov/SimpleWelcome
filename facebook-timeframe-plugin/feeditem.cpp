17da3cba31fa1b2a36be958cd52c557b82dd9a54
Julia Mineeva <julia.mineeva@osinit.ru>
2012-08-03 10:49:40 +0400

    added date support in facebook plugin

--- facebook-timeframe-plugin/feeditem.cpp
+++ facebook-timeframe-plugin/feeditem.cpp
@@ -1,11 +1,14 @@
 #include "feeditem.h"
 
+#include <QtCore/QDate>
 #include <QtCore/QDebug>
+
 #include <qjson/parser.h>
+
 
 QString FeedItem::pluginName() const
 {
-    return QLatin1String("Facebook");
+    return QLatin1String( "Facebook" );
 }
 
 FeedItem::Type FeedItem::type() const
@@ -18,37 +21,44 @@ QString FeedItem::id() const
     return m_id;
 }
 
-QVariant FeedItem::data(int role) const
+QVariant FeedItem::data( int role ) const
 {
-    return m_data.value(role);
+    return m_data.value( role );
+}
+
+QDate FeedItem::date() const
+{
+    return QDate::fromString( data( Date ).toString(), QString("d MM yyyy") );
 }
 
 void FeedItem::fillFromMap(QVariantMap map)
 {
     // http://developers.facebook.com/docs/reference/api/post/
-    if(map.contains("id"))
+    if ( map.contains( "id" ) )
         m_id = map.value("id").toString();
-    if(map.contains("message"))
-    {
-        QString message = map.value("message").toString();
+    if ( map.contains( "message" ) ) {
+        QString message = map.value( "message" ).toString();
 
         // if user posts a link
-        bool isLink = message.startsWith("http://");
-        if(isLink)
-        {
-            QString name = isLink ? map.value("name").toString() : message;
-            m_data.insert(Text, QString("<a href=%1>%2</a>").arg(message, name));
+        bool isLink = message.startsWith( "http://" );
+        if ( isLink ) {
+            QString name = isLink ? map.value( "name" ).toString() : message;
+            m_data.insert( Text, QString( "<a href=%1>%2</a>" ).arg( message, name ) );
         }
         else
-            m_data.insert(Text, message);
+            m_data.insert( Text, message );
     }
-    if(map.contains("picture"))
-        m_data.insert(ImageUrl, map.value("picture").toString());
+    if ( map.contains( "picture" ) )
+        m_data.insert( ImageUrl, map.value( "picture" ).toString() );
 
-    if(map.contains("story"))
-    {
-        m_data.insert(Text, map.value("story").toString());
+    if ( map.contains( "story" ) ) {
+        m_data.insert( Text, map.value( "story" ).toString() );
+    }
+    if ( map.contains( "created_time" ) ) {
+        QDateTime dt = map.value( "created_time" ).toDateTime();
+        QDate date = dt.date();
+        m_data.insert( Date, date.toString( "d MM yyyy" ) );
     }
 
-    m_data.insert(PluginName, pluginName());
+    m_data.insert( PluginName, pluginName() );
 }
