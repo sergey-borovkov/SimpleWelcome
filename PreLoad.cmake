694449c197eaf34b04b369037a27857283996d78
Dmitry Ashkadov <dmitry.ashkadov@rosalab.ru>
2012-08-13 15:27:51 +0400

    Fix: check for out-of-source build

--- PreLoad.cmake
+++
@@ -1,7 +0,0 @@
-#check out of source build
-get_filename_component(_src_dir_real_path "${CMAKE_SOURCE_DIR}" REALPATH)
-get_filename_component(_bin_dir_real_path "${CMAKE_BINARY_DIR}" REALPATH)
-
-if(${_src_dir_real_path} STREQUAL ${_bin_dir_real_path})
-	message(FATAL_ERROR "In-source builds are forbidden! Choose separate build directory.")
-endif()
