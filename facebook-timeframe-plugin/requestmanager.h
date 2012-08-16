3b1d02665f1c7a28822ac9fa3449f48f5d201e19
Sergey Borovkov <sergey.borovkov@osinit.ru>
2012-08-16 11:26:34 +0400

    Add interfaces for posting comments

--- facebook-timeframe-plugin/requestmanager.h
+++ facebook-timeframe-plugin/requestmanager.h
@@ -13,6 +13,8 @@ public:
     explicit RequestManager(QObject *parent = 0);
     virtual void queryWall(const QDate &beginDate, const QDate &endDate);
     virtual void queryImage(const QString &id);
+    virtual void postComment(const QString &parent);
+
     void setAuthorizer(OAuth2Authorizer *authorizer);
     void logout();
 
