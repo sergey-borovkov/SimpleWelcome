94a27f458688c6c4bf21bd498d18e418e02702c1
Sergey Borovkov <sergey.borovkov@osinit.ru>
2012-09-27 11:54:43 +0400

    Inherit LocalDayModel from ListModel

--- src/timeframe/local/localdayitem.h
+++ src/timeframe/local/localdayitem.h
@@ -1,11 +1,13 @@
 #ifndef GALLERYITEM_H
 #define GALLERYITEM_H
 
-#include <QObject>
-#include <QMetaType>
-#include <QStringList>
-#include <QDate>
-#include <QSortFilterProxyModel>
+#include <QtCore/QObject>
+#include <QtCore/QMetaType>
+#include <QtCore/QStringList>
+#include <QtCore/QDate>
+#include <QtGui/QSortFilterProxyModel>
+
+#include <listitem.h>
 
 class ItemModel;
 class Activity;
@@ -26,19 +28,33 @@ public slots:
 };
 
 
-class LocalDayItem : public QObject
+class LocalDayItem : public QObject, public ListItem
 {
     Q_OBJECT
 public:
+    enum {
+        CurrentDateRole = Qt::UserRole + 1,
+        ItemsRole,
+        CountRole,
+        TypesRole,
+        ItemsCountRole
+    };
+
     explicit LocalDayItem(const QDate &date, QObject *parent = 0);
     ~LocalDayItem();
 
-    QDate getDate();
-    int   getCount();
+    QString id() const;
+    virtual QVariant data(int role) const;
+    virtual bool setData(int role, const QVariant & value);
+
+    QDate getDate() const;
+    int   getCount() const;
 
     TimeFrameFilterModel * model();
     QString types() const;
     void thumbnailReady(QString);
+
+    static QHash<int, QByteArray> roleNames();
 
 signals:
     void dataChanged();
