f780beaa6920bfb196de0d1629a48d55bfe30eed
Sergey Borovkov <sergey.borovkov@osinit.ru>
2012-10-12 10:28:42 +0400

    Fix filter

--- src/timeframe/local/localcontentmodel.cpp
+++ src/timeframe/local/localcontentmodel.cpp
@@ -21,7 +21,7 @@ void LocalContentFilterModel::setFilter(const QString &filter)
     QRegExp filterRegExp;
     if (filter == "Local")
         filterRegExp = QRegExp("Image|Video|Doc");
-    else if (filter == "Photo")
+    else if (filter == "Image")
         filterRegExp = QRegExp("Image");
     else if (filter == "Video")
         filterRegExp = QRegExp("Video");
